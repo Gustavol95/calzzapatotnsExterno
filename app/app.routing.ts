ddf3710c38c8366de6e35866420723a633dc1998
IE Developer <tavols889@gmail.com>
2017-02-02 14:48:22 -0700

    Commit antes de merge

--- app/app.routing.ts
+++ app/app.routing.ts
@@ -2,9 +2,9 @@ import {LoginComponent} from "./pages/login/login.component";
 import {ModalViewComponent} from "./pages/login/modal/modal-view";
 import {InicioComponent} from "./pages/inicio/inicio.component";
 export const routes = [
-    {path: '', component: LoginComponent, useAsDefault: true},
+    {path: 'inicio', component: LoginComponent, useAsDefault: true},
     {path: 'recuperar-email', component: ModalViewComponent},
-    {path: 'incio', component: InicioComponent},
+    {path: '', component: InicioComponent},
 ];
 export const navigatableComponents = [
     LoginComponent,
