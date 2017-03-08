ceb43b05ee0295277ea34cac1175b9e71cb4879b
henry <heris161993@gmail.com>
2017-03-08 10:47:56 -0700

    Referencias bancarias
    Referencias bancarias

--- app/pages/corte/corte.component.ts
+++ app/pages/corte/corte.component.ts
@@ -4,6 +4,7 @@
 import {Component, OnInit} from "@angular/core";
 import {ActivatedRoute} from "@angular/router";
 import {Page} from "ui/page";
+import {RouterExtensions} from "nativescript-angular";
 @Component({
     selector: "corte",
     providers: [],
@@ -17,7 +18,7 @@ export class CorteComponent implements OnInit{
     pagoMinimo="";
 
 
-    constructor(private page:Page,private activatedRoute: ActivatedRoute){
+    constructor(private page:Page,private activatedRoute: ActivatedRoute,private routerExtensions: RouterExtensions){
         activatedRoute.queryParams.subscribe(params => {
             this.info = JSON.parse(params["info"]);
             this.saldo="$"+this.info.saldo;
@@ -29,4 +30,7 @@ export class CorteComponent implements OnInit{
         this.page.actionBar.title="Corte y Saldo";
     }
 
+    referenciabanc(){
+        this.routerExtensions.navigate(["/home/referenciabancaria"]);
+    }
 }
---
+++ app/pages/referenciasbancarias/referenciabancaria.service.ts
@@ -0,0 +1,11 @@
+import {Injectable} from "@angular/core";
+import "rxjs/add/operator/do";
+import "rxjs/add/operator/map";
+import {HttpService} from "../../custom-http/http-service";
+
+@Injectable()
+export class ReferenciabancariaService {
+
+    constructor(private http: HttpService) {
+    }
+}
