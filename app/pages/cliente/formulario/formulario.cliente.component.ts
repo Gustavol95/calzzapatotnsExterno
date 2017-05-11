32c5117fcaf5e6eaa62075e1624b5f07c7e7ed02
jaziel <jazimovil@gmail.com>
2017-05-11 08:55:18 -0600

    formato de fecha

--- app/pages/cliente/formulario/formulario.cliente.component.ts
+++ app/pages/cliente/formulario/formulario.cliente.component.ts
@@ -97,8 +97,8 @@ export class FormularioClienteComponent implements OnInit {
         // >> returning-result
         this._modalService.showModal(DatepickerComponent, options)
             .then((dateresult: Date) => {
-                this.form.get('fecha_nacimiento').setValue(moment(dateresult).format('DD/MM/YYYY'));
-                this.onTap('label7'); 
+                this.form.get('fecha_nacimiento').setValue(moment(dateresult).format('YYYY-MM-DD'));
+                this.onTap('label7');
             });
     }
 
