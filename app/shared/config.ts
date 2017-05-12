9b5daecbe61be781a4aa471bf7d7fd5f33a66dd4
Henry <heris161993@gmail.com>
2017-05-12 14:34:54 -0600

    Auto stash before merge of "develop" and "origin/develop"

--- app/shared/config.ts
+++ app/shared/config.ts
@@ -3,8 +3,7 @@ export class Config {
   //static apiUrl = "http://192.168.0.28/calzzapato_backend/public/api/";           /* ?????       */
   //static apiUrl = "http://192.168.125.160/calzzapato_backend/public/api/";      /* Henry Local */
   //static apiUrl = "http://lab.ie-soluciones.com/calzzapato/v1/api/";               /* Laboratorio */
-  //static apiUrl = "http://192.168.125.11/calzzapato_backend_laravel/public/api/";     /* Jaz Local */
-  static apiUrl = "http://192.168.125.244/calzzapato/v1/api/";                    /* Nuevo Lab   */
+  static apiUrl = "http://192.168.125.244/calzzapato/v1/api/";                     /* Nuevo Lab   */
 
   static token = "";
 }
